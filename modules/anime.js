81c6f716d0c3b80d05e225de2ebee300cc593c27
Subadra Poshitha <scienceposhitha@gmail.com>
2022-08-20 06:55:54 +0530

    Ai_Dark_Ezio is (try to fix err connet econnefused | forth time) ✔

--- modules/anime.js
+++ modules/anime.js
@@ -14,38 +14,28 @@ const ezio = require("../events");
 const axios = require("axios");
 // const lang = ezio.getString("github");
 
-// ezio.config.api.waifu.sfw.map(category => {
-//     ezio.addCommand( { pattern: [`anime-${category}`, `anime-${category}-gif`], sucReact: "🤹‍♀️", category: ["create",], }, async (message, client) => {
-//         await axios.get(`${ezio.config.api.waifu}/sfw/${category}`)
-//         .then(async (res) => {
-//             message.client.command == `anime-${category}`
-//             ? await client.sendFile(message.from, res.data.url, "", message, { asSticker: true, author: ezio.config.exif.author, packname: ezio.config.exif.packname, categories: ["😄", "😊"]})
-//             : await client.sendMessage( message.from, { video: { url: res.data.url }, caption: ezio.config.exif.footer, gifPlayback: true }, { quoted: message } );
-//             global.catchError = false;
-//         }).catch(async (err) => { global.catchError = true; await client.sendErrorMessage(message.from,ezio.errorMessage(err),message.key,message); });
-//     });
-// });
+ezio.config.api.waifu.sfw.map(category => {
+    ezio.addCommand( { pattern: [`anime-${category}`, `anime-${category}-gif`], sucReact: "🤹‍♀️", category: ["create",], }, async (message, client) => {
+        await axios.get(`${ezio.config.api.waifu.domain}/sfw/${category}`)
+        .then(async (res) => {
+            message.client.command == `anime-${category}`
+            ? await client.sendFile(message.from, res.data.url, "", message, { asSticker: true, author: ezio.config.exif.author, packname: ezio.config.exif.packname, categories: ["😄", "😊"]})
+            : await client.sendMessage( message.from, { image: { url: res.data.url }, caption: ezio.config.exif.footer, gifPlayback: true }, { quoted: message } );
+            global.catchError = false;
+        }).catch(async (err) => { global.catchError = true; await client.sendErrorMessage(message.from,ezio.errorMessage(err),message.key,message); });
+    });
+});
 
-// ezio.config.api.waifu.nsfw.map(category => {
-//     ezio.addCommand( { pattern: [`xanime-${category}`, `xanime-${category}-gif`], sucReact: "🔞", category: ["18+",], }, async (message, client) => {
-//         if (message.isGroup) { global.catchError = true; return await client.sendMessage( message.from, { text: ezio.errorMessage(ezio.config.reply.private) }, { quoted: message } ); };
-//         if (!message.client.isCreator) { global.catchError = true; return await client.sendMessage( message.from, { text: ezio.errorMessage(ezio.config.reply.owner) }, { quoted: message } ); };
-//         await axios.get(`${ezio.config.api.waifu}/nsfw/${category}`)
-//         .then(async (res) => {
-//             message.client.command == `anime-${category}`
-//             ? await client.sendFile(message.from, res.data.url, "", message, { asSticker: true, author: ezio.config.exif.author, packname: ezio.config.exif.packname, categories: ["😄", "😊"]})
-//             : await client.sendMessage( message.from, { video: { url: res.data.url }, caption: ezio.config.exif.footer, gifPlayback: true }, { quoted: message } );
-//             global.catchError = false;
-//         }).catch(async (err) => { global.catchError = true; await client.sendErrorMessage(message.from,ezio.errorMessage(err),message.key,message); });
-//     });
-// });
-
-ezio.addCommand( { pattern: [`anime`, `anime-gif`], sucReact: "🤹‍♀️", category: ["create",], }, async (message, client) => {
-    await axios.get(`https://api.waifu.pics/sfw/kill`)
-    .then(async (res) => {
-        message.client.command == `anime`
-        ? await client.sendFile(message.from, res.data.url, "", message, { asSticker: true, author: ezio.config.exif.author, packname: ezio.config.exif.packname, categories: ["😄", "😊"]})
-        : await client.sendMessage( message.from, { video: { url: res.data.url }, caption: ezio.config.exif.footer, gifPlayback: true }, { quoted: message } );
-        global.catchError = false;
-    }).catch(async (err) => { global.catchError = true; await client.sendErrorMessage(message.from,ezio.errorMessage(err),message.key,message); });
+ezio.config.api.waifu.nsfw.map(category => {
+    ezio.addCommand( { pattern: [`xanime-${category}`, `xanime-${category}-gif`], sucReact: "🔞", category: ["18+",], }, async (message, client) => {
+        if (message.isGroup) { global.catchError = true; return await client.sendMessage( message.from, { text: ezio.errorMessage(ezio.config.reply.private) }, { quoted: message } ); };
+        if (!message.client.isCreator) { global.catchError = true; return await client.sendMessage( message.from, { text: ezio.errorMessage(ezio.config.reply.owner) }, { quoted: message } ); };
+        await axios.get(`${ezio.config.api.waifu.domain}/nsfw/${category}`)
+        .then(async (res) => {
+            message.client.command == `anime-${category}`
+            ? await client.sendFile(message.from, res.data.url, "", message, { asSticker: true, author: ezio.config.exif.author, packname: ezio.config.exif.packname, categories: ["😄", "😊"]})
+            : await client.sendMessage( message.from, { image: { url: res.data.url }, caption: ezio.config.exif.footer, gifPlayback: true }, { quoted: message } );
+            global.catchError = false;
+        }).catch(async (err) => { global.catchError = true; await client.sendErrorMessage(message.from,ezio.errorMessage(err),message.key,message); });
+    });
 });
