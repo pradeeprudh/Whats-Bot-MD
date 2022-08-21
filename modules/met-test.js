ab06d07c449b52021489c73c6f7befe9970a8689
Subadra Poshitha <scienceposhitha@gmail.com>
2022-08-21 21:43:23 +0530

    Ai_Dark_Ezio is (test some contest info) ✔

--- modules/met-test.js
+++ modules/met-test.js
@@ -24,12 +24,35 @@ ezio.addCommand(
     const caption = `------- WhatsApp Groups -------`;
     await client.sendMessage(
       message.from,
-      { text: caption },
       {
-        quoted: {
-          key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast", } : {}), },
-          message: { imageMessage: { url: "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", mimetype: "image/jpeg", caption: "Dark Ezio", fileSha256: "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", fileLength: "28777", height: 1080, width: 1079, mediaKey: "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", fileEncSha256: "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", directPath: "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", mediaKeyTimestamp: "1610993486", jpegThumbnail: fs.readFileSync(path.join(__dirname, '..', 'database', "image", "Test_Group_Qr.jpeg")), }, },
-        },
+        text: caption,
+        contextInfo: {
+           externalAdReply: {
+
+        /** ExternalAdReplyInfo title */
+        title: "Title",
+
+        /** ExternalAdReplyInfo body */
+        body: 'body',
+
+        /** ExternalAdReplyInfo mediaType */
+        mediaType: 1,
+
+        /** ExternalAdReplyInfo mediaUrl */
+        mediaUrl: ezio.config.image.url.D_E_DPC,
+
+        /** ExternalAdReplyInfo sourceUrl */
+        sourceUrl: ezio.config.exif.web,
+
+        /** ExternalAdReplyInfo containsAutoReply */
+        containsAutoReply: true,
+
+        /** ExternalAdReplyInfo showAdAttribution */
+        showAdAttribution: true
+    }}
+      },
+      {
+        quoted: message,
       }
     );
     global.catchError = false;
