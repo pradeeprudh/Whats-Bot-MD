3d629d377b910cf474f35d6ebf2d9f8dcf64bc98
Subadra Poshitha <scienceposhitha@gmail.com>
2022-08-25 06:36:31 +0530

    Ai_Dark_Ezio is (testing qutod url previwe message seventh time) ✔

--- modules/met-test.js
+++ modules/met-test.js
@@ -21,20 +21,40 @@ ezio.addCommand(
     sucReact: "🎟",
   },
   async (message, client) => {
-    const caption = `------- WhatsApp Groups ------- `;
+    const caption = `------- WhatsApp Groups ------- https://aidarkezio.github.io`;
     const Message = {
       linkPreview: {
-        'canonical-url': 'https://aidarkezio.github.io',
-        'matched-text': '',
+        "canonical-url": "https://aidarkezio.github.io",
+        "matched-text": "https://aidarkezio.github.io",
         title: "Subhadra Poshi",
-        description: 'This is a WhatsApp user bot \n\nSend this number: Alive',
-        jpegThumbnail: ezio.config.image.encd.D_E_ADEC
+        description: "This is a WhatsApp user bot \n\nSend this number: Alive",
+        jpegThumbnail: ezio.config.image.encd.D_E_ADEC,
       },
-      text: caption
-    }
+      text: caption,
+    };
+    const Message2 = {
+      text: teks,
+      contextInfo: {
+        externalAdReply: {
+          title: 'global.botname}',
+          body: ` Gojo-Satoru`,
+          previewType: "PHOTO",
+          thumbnailUrl: ``,
+          thumbnail: ezio.config.image.encd.D_E_TMB,
+          sourceUrl: "https://telegra.ph/file/8bbe8a7de5c351dfcb077.jpg",
+        },
+      },
+    };
     await client.sendMessage(
       message.from,
       Message,
+      {
+        quoted: ezio.config.quoted.product,
+      }
+    );
+    await client.sendMessage(
+      message.from,
+      Message2,
       {
         quoted: ezio.config.quoted.product,
       }
